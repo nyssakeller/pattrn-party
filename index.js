14d66c6fde07ec53f985a83ce601668fba1b85e9
Nyssa Keller <nyssa1@gmail.com>
2018-03-27 09:42:40 -0600

    Get active cardsto reflect the clicked tab

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const charlieBtn = document.querySelector('.charlie-btn');
-const tabBtn = document.querySelectorAll('.tab-btn');
-const activeArr = [charlieBtn];
+const activeArr = [document.querySelector('.charlie-btn')];
+const activeCard = [document.querySelector('.tab1')]; 
 
-const toggleClass = (e) => {
+document.querySelector('.charlie-btn').classList.add('active');
+document.querySelector('.tab1').classList.add('active-card');
+
+const toggleClass = e => {
   activeArr.shift().classList.remove('active');
-  activeArr.push(e.target)
-  activeArr.forEach(button => button.classList.toggle('active'))
+  activeArr.push(e.target);
+  activeArr.forEach(button => {
+    button.classList.toggle('active')
+  });
+  showCard(e);
 }
 
-tabBtn.forEach(button => {
+const showCard = e => {
+  activeCard.shift().classList.remove('active-card');
+  activeCard.push(document.querySelector(`.tab${e.target.id}`));
+  activeCard.forEach(card => card.classList.toggle('active-card'));
+}
+
+document.querySelectorAll('.tab-btn').forEach(button => {
   button.addEventListener('click', toggleClass)
 });
 
