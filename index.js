4b878a6a39d70f3dd98e8abe7ecb5996745e4570
Nyssa Keller <nyssa1@gmail.com>
2018-03-27 08:48:27 -0600

    Get active tabs working. Cards dont reflect this yet

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hi');
+const charlieBtn = document.querySelector('.charlie-btn');
+const tabBtn = document.querySelectorAll('.tab-btn');
+const activeArr = [charlieBtn];
+
+const toggleClass = (e) => {
+  activeArr.shift().classList.remove('active');
+  activeArr.push(e.target)
+  activeArr.forEach(button => button.classList.toggle('active'))
+}
+
+tabBtn.forEach(button => {
+  button.addEventListener('click', toggleClass)
+});
+
+
+
